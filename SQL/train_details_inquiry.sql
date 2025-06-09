bb6623e6bafe8097783b6bbe7ae4bf56f8d76ffb
songok14 <rlathddhr11@gmail.com>
2025-06-09 11:21:27 +0900

    first

---
+++ SQL/train_details_inquiry.sql
@@ -0,0 +1,13 @@
+-- 같은 schedule_seq 내 출발/도착 한 줄에 출력
+select s1.departure as '출발지', s1.departure_time as '출발시간',
+    s2.destination as '도착지', s2.destination_time as '도착시간'
+from schedules_detail as s1
+left join schedules_detail s2 on s1.schedules_seq = s2.schedules_seq
+where s1.schedules_seq = 1 and s1.departure = '서울' and s2.destination = '부산';
+
+-- 출발지가 서울인 모든 도착지 정보
+select s1.departure as '출발지', s1.departure_time as '출발시간',
+    s2.destination as '도착지', s2.destination_time as '도착시간'
+from schedules_detail as s1
+left join schedules_detail s2 on 1
+where s1.departure = '서울' and s1.departure_time <= s2.destination_time;
